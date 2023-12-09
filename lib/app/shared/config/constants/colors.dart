addbb8c66c4e0920fb952032cf32e1c29d8bd869
YashMakan <yashmakan.work@gmail.com>
2023-12-09 13:07:25 +0530

    Created Home Page UI Changed minSdkVersion to 19

--- lib/app/shared/config/constants/colors.dart
+++ lib/app/shared/config/constants/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  static const Color primary = Colors.blue;
+  static const Color primary = Color(0xFF49d7b3);
+
+  static const Color darkGrey = Color(0xFF1a262c);
 }
